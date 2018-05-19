90920d34e327e0452ba04f61cf950a3baf888415
JimmyLambert <lambert.66@wright.edu>
2018-05-19 12:27:33 -0400

    Added Project

---
+++ WpfApplication4/LotButton.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApplication4
+{
+	class LotButton : Button
+	{
+		string Path;
+		string LabelName;
+
+		public LotButton()
+		{
+			
+		}
+
+		private void LotButton_Click(WebBrowser wb)
+		{
+			wb.Source = new Uri(this.Path);
+		}
+
+		public partial class LotButtonDialog : Window
+		{
+			Label NameLabel;
+			TextBox NameTextBox;
+			Label FileLabel;
+			Label DialogResultLabel;
+			Button BrowseButton;
+			Button CreateButton;
+			Button CancelButton;
+			Grid MainGrid;
+			public LotButtonDialog()
+			{
+				this.AddChild(MainGrid);
+
+				MainGrid.Margin = new Thickness(10);
+				MainGrid.RowDefinitions.Add(new RowDefinition());
+				MainGrid.RowDefinitions.Add(new RowDefinition());
+				MainGrid.RowDefinitions.Add(new RowDefinition());
+				MainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+				MainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+				MainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+				NameLabel = new Label();
+				NameLabel.Content = "Enter Name: ";
+				MainGrid.Children.Add(NameLabel);
+				Grid.SetColumn(NameLabel, 1);
+				Grid.SetRow(NameLabel, 0);
+
+				NameTextBox = new TextBox();
+				MainGrid.Children.Add(NameTextBox);
+				Grid.SetColumnSpan(NameTextBox, 2);
+				Grid.SetRow(NameLabel, 0);
+
+				FileLabel = new Label();
+				FileLabel.Content = "Path to File: ";
+				MainGrid.Children.Add(FileLabel);
+				Grid.SetColumn(FileLabel, 0);
+				Grid.SetRow(FileLabel, 1);
+
+				DialogResultLabel = new Label();
+				MainGrid.Children.Add(DialogResultLabel);
+				Grid.SetColumn(DialogResultLabel ,1);
+				Grid.SetRow(DialogResultLabel, 1);
+
+				BrowseButton = new Button();
+				BrowseButton.Content = "Browse...";
+				MainGrid.Children.Add(BrowseButton);
+				Grid.SetColumn(BrowseButton, 1);
+				Grid.SetRow(BrowseButton, 2);
+
+				CreateButton = new Button();
+				CreateButton.Content = "Create";
+				MainGrid.Children.Add(CreateButton);
+				Grid.SetColumn(CreateButton, 2);
+				Grid.SetRow(CreateButton, 1);
+
+				CancelButton = new Button();
+				CancelButton.Content = "Cancel";
+				MainGrid.Children.Add(CancelButton);
+				Grid.SetColumn(CancelButton, 2);
+				Grid.SetRow(CancelButton, 2);
+			}
+			private void CreateButton_Click(object sender, RoutedEventArgs e)
+			{
+
+			}
+		}
+		//bool validEntry = false;
+		//	do
+		//	{
+		//		LotButton newLotButton = new LotButton();
+		//		if(newLotButton.GetName().Equals(oldLotButton.GetName())))
+		//		{
+		//			MessageBox.Show("Error, Name Already Taken. Try Again.");
+		//		}
+		//		else
+		//			validEntry=true;
+		//	} while (validEntry == false);
+
+
+
+		//	Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+		//	dlg.ShowDialog();
+		//	try
+		//	{
+		//		LotButtons.Add(new LotButton(@"C:\Users\Developer\Desktop\This is a purchase order.pdf"));
+		//		LotButtonStackPanel.Children.Add(LotButtons.Last());
+		//	}
+		//	catch(Exception ex)
+		//	{
+		//		MessageBox.Show(ex.Message);
+		//	}
+			
+	}
+}
---
+++ WpfApplication4/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication4
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void AddLotMenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			LotsGrid.ColumnDefinitions.Add(new ColumnDefinition());
+		}
+
+		private void LotMenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			//LotsGrid.Children.Add();
+			
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			LotWindow window = new LotWindow();
+			window.ShowDialog();
+			window.Activate();
+		}
+
+	}
+}
